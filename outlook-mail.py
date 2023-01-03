211f18d0693723feaa866502ea3f58903cee0b4b
atrox3d <atrox3d@gmail.com>
2023-01-03 17:32:41 +0100

    55. Send a Rich HTML Email from Outlook or Hotmail

--- outlook-mail.py
+++ outlook-mail.py
@@ -1,5 +1,6 @@
 import smtplib
-import os
+from email.mime.text import MIMEText
+from email.mime.multipart import MIMEMultipart
 
 from secret import outlook_mail as om
 from secret import mail_recipients as mr
@@ -7,13 +8,21 @@ from secret import mail_recipients as mr
 sender = om.USER
 receiver = mr.GMAIL
 subject = 'Hello hello'
-body= """
-hello from outlook
+
+message = MIMEMultipart()
+message['From'] = sender
+message['To'] = receiver
+message['Subject'] = 'html message'
+
+body = """
+<h1>hello from outlook</h1>
 """
-message = f"Subject: {subject}\n{body}"
+mimetext = MIMEText(body, 'html')
+message.attach(mimetext)
 
 server = smtplib.SMTP('smtp.office365.com', 587)
 server.starttls()
 server.login(om.USER, om.PASSWORD)
-server.sendmail(om.USER, mr.GMAIL, message)
+print(message.as_string())
+server.sendmail(om.USER, mr.GMAIL, message.as_string())
 server.quit()
